dd4c268eac2c2df130e3fc1a1841ec4d0473a0dd
jondashkyle <contact@jon-kyle.com>
2018-04-11 22:44:10 -0700

    copy

--- source/views/berlin/berlin-two/index.js
+++ source/views/berlin/berlin-two/index.js
@@ -20,6 +20,15 @@ function view (state, emit) {
   var page = new Page(state)
   var venue = page().value('venue')
 
+  var lang = {
+    en: {
+      text: page('/about').value('texten')
+    },
+    de: {
+      text: page('/about').value('textde')
+    }
+  }
+
   return html`
     <div>
       <div class="vhmn100 x xdc xjb c12 lh1-5 fs1">
@@ -27,29 +36,37 @@ function view (state, emit) {
         <div class="psr x xx xdc oh">
           <div class="xx x xdc xjb p0-5 z2 fc-white">
             <div class="lh1 fsvw12 pen p0-5">${page().value('intro')}</div>
-            <div class="c12 sm-c6 fs2 co0 sm-co6 lh1-25 p0-5 mt4">
-              ${objectValues(page().value('speakers'))
-                .map(function (speaker) {
-                  return html`<div>
-                    <a href="${speaker.href}" target="_blank">${speaker.name}</a>
-                    <span class="external"></span>
-                  </div>`
-                })
-              }
-            </div>
-            <div class="x xw mt4 fs2">
-              <div class="c12 sm-c6 lh1-25 ff-mono p0-5">
-                ${page()
-                  .value('introtags')
-                  .map(function (tag) {
-                    return html`<div>#${tag}</div>`
+            <div class="x xw c12">
+              <div class="mb4 sm-b0 c12 sm-c6 psr">
+                ${createExtra({
+                  position: positions.smile,
+                  source: page().file('happy.png').value('path')
+                })}
+              </div>
+              <div class="c12 sm-c6 fs2 co0 lh1-25 p0-5 mt4">
+                ${objectValues(page().value('speakers'))
+                  .map(function (speaker) {
+                    return html`<div>
+                      <a href="${speaker.href}" target="_blank">${speaker.name}</a>
+                      <span class="external"></span>
+                    </div>`
                   })
                 }
               </div>
-              <div class="c12 sm-c6 lh1-25 p0-5">
-                <div>${venue.name}</div>
-                <div><a href="${venue.map}">${venue.address}</a></div>
-                <div>${page().value('title')} @ ${page().value('time')}</div>
+              <div class="c12 x xw mt4 fs2">
+                <div class="c12 sm-c6 lh1-25 ff-mono p0-5">
+                  ${page()
+                    .value('introtags')
+                    .map(function (tag) {
+                      return html`<div>#${tag}</div>`
+                    })
+                  }
+                </div>
+                <div class="c12 sm-c6 lh1-25 p0-5">
+                  <div>${venue.name}</div>
+                  <div><a href="${venue.map}">${venue.address}</a></div>
+                  <div>${page().value('title')} @ ${page().value('time')}</div>
+                </div>
               </div>
             </div>
           </div>
@@ -61,15 +78,20 @@ function view (state, emit) {
             })
           }
         </div>
-        <div class="c12 p0-5 x xjc bgc-white">
-          <div class="c12 sm-c6 fs2 p0-5">
-            ${state
-              .cache(FormRsvp, 'berlin-rsvp')
-              .render({
-                event: 'berlin-2',
-                lang: state.ui.lang
-              })
-            }
+        <div class="c12 p0-5 bgc-white">
+          <div class="x xw mxa mxwidth">
+            <div class="c12 sm-c6 p0-5 fc-black copy">
+              ${lang[state.ui.lang].text.split('\n')[0]} <a href="/about">Continue reading…</a>
+            </div>
+            <div class="c12 sm-c6 fs2 p0-5">
+              ${state
+                .cache(FormRsvp, 'berlin-rsvp')
+                .render({
+                  event: 'berlin-2',
+                  lang: state.ui.lang
+                })
+              }
+            </div>
           </div>
         </div>
       </div>
