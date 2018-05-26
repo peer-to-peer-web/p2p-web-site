f7aa94dab8df0257f24c72843db4907b00e572fd
jondashkyle <contact@jon-kyle.com>
2018-05-25 17:16:14 -0700

    livestream fallback

--- source/plugins/livestream.js
+++ source/plugins/livestream.js
@@ -1,5 +1,6 @@
 var WebSocket = require('../lib/ws')
 var xtend = require('xtend')
+var xhr = require('xhr')
 
 module.exports = plugin
 
@@ -10,6 +11,7 @@ function plugin (opts) {
       live: false,
       loaded: false,
       fallback: false,
+      http: 'https://p2p-web-livestream.glitch.me?' + Math.floor(new Date () / 1000),
       address: 'wss://p2p-web-livestream.glitch.me',
       source: 'https://broadcast.sh:4433/hls/b73bg37478bkb9b/index.m3u8',
       title: 'NYC'
@@ -20,6 +22,17 @@ function plugin (opts) {
 
     function handleLoad () {
       ws = new WebSocket(state.livestream.address)
+
+      ws.addEventListener('error', function (event) {
+        xhr(state.livestream.http, function (err, res, body) {
+          try {
+            var data = JSON.parse(body)
+            state.livestream = xtend(state.livestream, data)
+          } catch (err) { }
+          state.livestream.loaded = true
+          emitter.emit(state.events.RENDER)
+        })
+      })
 
       ws.addEventListener('open', function (event) {
         state.livestream.loaded = true
